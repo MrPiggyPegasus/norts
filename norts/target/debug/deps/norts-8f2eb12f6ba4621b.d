0211ae1df4ef2f8b294798d4f5fc121b3d6cef6d
MrPiggyPegasus <121462217+MrPiggyPegasus@users.noreply.github.com>
2023-04-12 21:44:24 +0100

    Delete norts/target directory

--- norts/target/debug/deps/norts-8f2eb12f6ba4621b.d
+++
@@ -1,8 +0,0 @@
-/home/mrpiggypegasus/dev/norts/norts/target/debug/deps/norts-8f2eb12f6ba4621b.rmeta: src/lib.rs src/bitboards.rs src/board.rs src/search.rs
-
-/home/mrpiggypegasus/dev/norts/norts/target/debug/deps/norts-8f2eb12f6ba4621b.d: src/lib.rs src/bitboards.rs src/board.rs src/search.rs
-
-src/lib.rs:
-src/bitboards.rs:
-src/board.rs:
-src/search.rs:
--- norts/target/debug/deps/norts-d262f0e8f8acd461.d
+++
@@ -1,8 +0,0 @@
-/home/mrpiggypegasus/dev/norts/norts/target/debug/deps/norts-d262f0e8f8acd461.rmeta: src/lib.rs src/bitboards.rs src/board.rs src/search.rs
-
-/home/mrpiggypegasus/dev/norts/norts/target/debug/deps/norts-d262f0e8f8acd461.d: src/lib.rs src/bitboards.rs src/board.rs src/search.rs
-
-src/lib.rs:
-src/bitboards.rs:
-src/board.rs:
-src/search.rs:
